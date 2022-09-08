e9452514f243514e7df9e0fabdccd18f818cb119
Asma-Ha <ja_hamidi@esi.dz>
2022-09-08 18:22:55 +0100

    styles

--- registerpage.php
+++ registerpage.php
@@ -1,54 +1,12 @@
 <html>
     <head>
         <title> registration form </title>
+        <link rel="stylesheet" type="text/css" href="styles.css" />
     </head>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Cairo:wght@400;800&display=swap');
-        body {
-            font-family: 'Cairo', sans-serif;
-        }
-        input {
-            border: none;
-        }
-        #form-header {
-            text-align : center;
-            font-size : 30px;
-            margin : 10px;
-        }
-
-        #register-form {
-            margin-top : 40px;
-        }
-        .container {
-            margin :20px 25%;
-            background-color : #B6D0E2;
-            padding : 10px 30px;
-            
-        }
-        #registration-container {
-            text-align : center;
-            margin-top : 4.9em;
-        }
-
-        .submit-button {
-            float : right;
-            margin : 10px 2px;
-            padding : 3px 14px;
-            background-color : #7fadcc;
-            font-size : 12px;
-            font-family: 'Cairo', sans-serif;
-        }
-
-        .text-zone {
-            padding : 6px;
-            float:right;
-        }
-
-    </style>
     <body>
         <div class="container">
             <h3 id="form-header"> Register </h3>
-            <form id="register-form" action="" method="POST">
+            <form action="" method="POST">
 
                 <label for="userame"> Username </label>
                 <input class= "text-zone" type="text" id="username" name="username" value="" required>
